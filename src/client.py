e8529eb9680e1eb68f1925d6392d4c370193f80c
TrixiS <oficialmorozov@gmail.com>
2020-11-04 16:37:49 +0500

    Установил валидацию для полей ввода логина и пароля

--- src/client.py
+++ src/client.py
@@ -1,3 +1,5 @@
+import sys
+
 from PyQt5 import uic
 from qtwidgets import PasswordEdit
 from PyQt5.QtWidgets import (
@@ -5,6 +7,9 @@ from PyQt5.QtWidgets import (
     QLineEdit, QPushButton,
     QLabel
 )
+
+from PyQt5.QtGui import QRegExpValidator
+from PyQt5.QtCore import QRegExp
 
 from .utils.paths import get_dir
 from .utils import constants
@@ -40,11 +45,17 @@ class LoginDialog(QDialog):
         self.password_edit = PasswordEdit(True, self)
         self.login_button = QPushButton(constants.LOGIN_BUTTON_TEXT, self)
         self.register_button = QPushButton(constants.REGISTER_BUTTON_TEXT, self)
-
         self.login_label.resize(self.login_label.sizeHint())
         self.password_label.resize(self.password_label.sizeHint())
         self.register_button.resize(self.register_button.sizeHint())
         self.login_button.resize(self.register_button.size())
+
+        self.login_edit.setValidator(QRegExpValidator(
+            QRegExp("[a-zA-Z]*", False)
+        ))
+        self.password_edit.setValidator(QRegExpValidator(
+            QRegExp("[^\s]*", False)
+        ))
 
         self.login_edit.move(
             constants.LOGIN_WIDGET_X_OFFSET,
@@ -79,6 +90,23 @@ class LoginDialog(QDialog):
             constants.LOGIN_WIDGET_X_OFFSET,
             offset_y(self.password_edit, offset=0, down=False)
         )
+
+        self.accepted.connect(self.accepted_event)
+        self.rejected.connect(self.refected_event)
+        self.login_button.clicked.connect(self.login_clicked)
+        self.register_button.clicked.connect(self.register_clicked)
+
+    def refected_event(self):
+        sys.exit()
+
+    def accepted_event(self):
+        self.close()
+
+    def login_clicked(self):
+        self.accept()
+
+    def register_clicked(self):
+        self.accept()
 
 
 class MainWindow(QMainWindow):
